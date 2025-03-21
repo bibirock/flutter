9760d4a1a15a2aa1246310c9772ee95cdcbf770e
Joe.Chen <joechen@tracle-tw.com>
2025-03-21 14:45:54 +0800

    新增本地化支持，刪除不必要的圖片，並更新 pubspec.yaml 以包含相關依賴

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@
  * @Author: Joe.Chen
  * @Date: 2025-03-12 18:10:09
  * @LastEditors: Joe.Chen joechen@tracle-tw.com
- * @LastEditTime: 2025-03-21 14:01:32
+ * @LastEditTime: 2025-03-21 14:45:02
  * @Description: 
  */
+
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'services/settings/api_service_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,6 +15,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'providers/auth_provider.dart';
 import 'screens/login/login_screen.dart';
 import 'screens/home/home_screen.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -46,6 +50,18 @@ class _TracleDriverAppState extends ConsumerState<TracleDriverApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: const Color(0xFF9AC972)),
+      // 添加本地化支持
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('zh', ''), // Traditional Chinese
+      ],
+      locale: const Locale('zh'),
+
       home: AnimatedSwitcher(
         duration: const Duration(milliseconds: 300),
         transitionBuilder: (Widget child, Animation<double> animation) {
